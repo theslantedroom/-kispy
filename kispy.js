7b8856974c54d34e03b45dbe6cf2d079facf8cd5
Steve Yee <steveplaysaxe@hotmail.com>
2022-09-28 19:03:35 -0700

    push update

--- kispy.js
+++ kispy.js
@@ -1,10 +1,13 @@
 const meowSound = document.getElementById("meowSound");
 const imKispySfx = document.getElementById("imKispySfx");
+const trustSfx = document.getElementById("trustSfx");
 imKispySfx.preload = "auto";
 meowSound.preload = "auto";
+trustSfx.preload = "auto";
 
 let tapCount = 0;
 let headSpinCounter = 1;
+let reverseHeadSpin = false;
 
 let images = [];
 function preload() {
@@ -25,34 +28,87 @@ preload(
   "assets/kispyPng8.png"
 );
 
-const advanceHeadSpin = () => {
-  if (headSpinCounter == 8) {
-    headSpinCounter = 1;
+const playSfx = (sound) => {
+  return new Promise(function (resolve, reject) {
+    sound.play();
+    setTimeout(() => {
+      resolve();
+    }, sound.duration * 1000);
+  });
+};
+
+const rollHead = () => {
+  if (headSpinCounter >= 8) {
+    reverseHeadSpin = true;
+  }
+  if (headSpinCounter <= 1) {
+    reverseHeadSpin = false;
   }
   kispy.style.backgroundImage =
     "url(assets/kispyPng" + headSpinCounter + ".png)";
+  headSpinCounter += reverseHeadSpin ? -1 : 1;
+};
+
+const spinHeadNotch = () => {
+  kispy.style.backgroundImage =
+    "url(assets/kispyPng" + headSpinCounter + ".png)";
   headSpinCounter += 1;
+  if (headSpinCounter >= 9) {
+    headSpinCounter = 1;
+  }
+};
+
+const jump = () => {
+  return new Promise(function (resolve, reject) {
+    let jump = setInterval(function () {
+      kispy.classList.add("jump");
+    }, 50);
+    setTimeout(function () {
+      kispy.classList.remove("jump");
+      clearInterval(jump);
+      resolve();
+    }, 500);
+  });
 };
 
 const headSpin = () => {
   let chase = setInterval(function () {
-    advanceHeadSpin();
+    spinHeadNotch();
   }, 50);
-
   setTimeout(function () {
     clearInterval(chase);
   }, 1000);
 };
 
-const fun = () => {
+const headRoll = (time = 800) => {
+  let chase = setInterval(function () {
+    rollHead();
+  }, 50);
+  setTimeout(function () {
+    clearInterval(chase);
+  }, time);
+};
+
+const meow = async () => {
+  playSfx(meowSound);
   headSpin();
-  meow();
 };
 
 const talk = () => {
-  advanceHeadSpin();
-  imKispySfx.play();
+  spinHeadNotch();
+  playSfx(imKispySfx);
 };
-const meow = () => {
-  meowSound.play();
+
+const trustMe = () => {
+  trustSfx.play();
+  headSpin();
+};
+const speak = async () => {
+  spinHeadNotch();
+  await playSfx(imKispySfx);
+  spinHeadNotch();
+  await playSfx(meowSound);
+  headSpin();
+  jump();
+  await playSfx(trustSfx);
 };
