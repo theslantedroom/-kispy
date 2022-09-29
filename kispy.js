22859c448ccf9a31645202dad322d90c09ae360a
Steve Yee <steveplaysaxe@hotmail.com>
2022-09-28 18:05:37 -0700

    push update

--- kispy.js
+++ kispy.js
@@ -1,8 +1,29 @@
 const meowSound = document.getElementById("meowSound");
 const imKispySfx = document.getElementById("imKispySfx");
+imKispySfx.preload = "auto";
+meowSound.preload = "auto";
 
 let tapCount = 0;
 let headSpinCounter = 1;
+
+let images = [];
+function preload() {
+  for (var i = 0; i < arguments.length; i++) {
+    images[i] = new Image();
+    images[i].src = preload.arguments[i];
+  }
+}
+
+preload(
+  "assets/kispyPng1.png",
+  "assets/kispyPng2.png",
+  "assets/kispyPng3.png",
+  "assets/kispyPng4.png",
+  "assets/kispyPng5.png",
+  "assets/kispyPng6.png",
+  "assets/kispyPng7.png",
+  "assets/kispyPng8.png"
+);
 
 const advanceHeadSpin = () => {
   if (headSpinCounter == 8) {
